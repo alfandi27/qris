6add5e773f93908739945b9af2f9669f62d4b5e1
alfandi27 <63239402+alfandi27@users.noreply.github.com>
2025-05-19 15:50:32 +0700

    Update qris.js

--- qris.js
+++ qris.js
@@ -1,4 +1,14 @@
-
+function debounce(func, wait) {
+    let timeout;
+    return function executedFunction(...args) {
+        const later = () => {
+            clearTimeout(timeout);
+            func(...args);
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+}
 
 // Countdown function
     function startCountdown(expiredDate) {
